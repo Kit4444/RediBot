671b1810ba83a84d3d7a8cd04c696c6e9ec77f8a
Maurice Bailey <Maurice Bailey@192.168.0.11>
2020-12-06 22:16:50 +0100

    LOA command is now synced with RediMain and RediStaff

--- RediBot_new/src/at/mlps/botclasses/commands/LOA.java
+++ RediBot_new/src/at/mlps/botclasses/commands/LOA.java
@@ -3,6 +3,7 @@ package at.mlps.botclasses.commands;
 import java.util.concurrent.TimeUnit;
 
 import at.mlps.main.Main;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.Role;
@@ -17,14 +18,19 @@ public class LOA extends ListenerAdapter{
 		String cont = msg.getContentRaw();
 		Role staff = e.getGuild().getRoleById(552161168412639235L);
 		Role loa = e.getGuild().getRoleById(574642929289789440L);
+		long mem = e.getMember().getIdLong();
 		if(cont.equalsIgnoreCase(Main.botprefix + "loa")) {
+			Guild staffguild = e.getJDA().getGuildById(612372586386423824L);
+			Role loa_staff = staffguild.getRoleById(784900192175783977L);
 			if(e.getMember().getRoles().contains(staff)) {
 				if(e.getMember().getRoles().contains(loa)) {
 					e.getGuild().removeRoleFromMember(e.getMember(), loa).complete();
 					e.getMessage().addReaction("approved:678036504391581730").queue();
+					staffguild.removeRoleFromMember(mem, loa_staff).queue();
 				}else {
 					e.getGuild().addRoleToMember(e.getMember(), loa).complete();
 					e.getMessage().addReaction("approved:678036504391581730").queue();
+					staffguild.addRoleToMember(mem, loa_staff).queue();
 				}
 			}else {
 				e.getMessage().addReaction("no:642212652566839326").queue();
