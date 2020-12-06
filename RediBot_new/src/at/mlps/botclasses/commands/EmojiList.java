e38aaa042f1bd5bf945c1e33397bfe4ea2ebd0d4
Maurice Bailey <Maurice Bailey@192.168.0.11>
2020-12-06 22:15:54 +0100

    Added a emojilist command.

---
+++ RediBot_new/src/at/mlps/botclasses/commands/EmojiList.java
@@ -0,0 +1,55 @@
+package at.mlps.botclasses.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import at.mlps.main.Main;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Emote;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class EmojiList extends ListenerAdapter{
+	
+	public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
+		String cont = e.getMessage().getContentRaw();
+		Member m = e.getMember();
+		Guild g = e.getGuild();
+		TextChannel chan = e.getChannel();
+		if(cont.equalsIgnoreCase(Main.botprefix + "emojilist")) {
+			List<Emote> nonAnim = new ArrayList<>();
+			List<Emote> Anim = new ArrayList<>();
+			for(Emote em : g.getEmotes()) {
+				if(em.isAnimated()) {
+					Anim.add(em);
+				}else {
+					nonAnim.add(em);
+				}
+			}
+			EmbedBuilder ebAnim = new EmbedBuilder();
+			EmbedBuilder ebNonAnim = new EmbedBuilder();
+			StringBuilder sbAnim = new StringBuilder();
+			StringBuilder sbNonAnim = new StringBuilder();
+			for(Emote em : nonAnim) {
+				sbNonAnim.append(em.getAsMention() + " - ``:" + em.getName() + ":``");
+				sbNonAnim.append("\n");
+			}
+			for(Emote em : Anim) {
+				sbAnim.append(em.getAsMention() + " - ``:" + em.getName() + ":``");
+				sbAnim.append("\n");
+			}
+			ebNonAnim.setDescription(sbNonAnim.toString());
+			ebAnim.setDescription(sbAnim.toString());
+			ebNonAnim.setColor(m.getColor());
+			ebAnim.setColor(m.getColor());
+			ebNonAnim.setAuthor("Static Emojis", g.getIconUrl());
+			ebAnim.setAuthor("Animated Emojis", g.getIconUrl());
+			chan.sendMessage(ebNonAnim.build()).queue();
+			chan.sendMessage(ebAnim.build()).queue();
+		}
+	}
+
+}
